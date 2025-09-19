f917e630252a3b850130fcb8eb7cac70b120dc95
Muhammad Affandes <affandes@uin-suska.ac.id>
2025-09-19 03:27:51 +0000

    Ubah lagi

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String nama1 = "Affan";
         int a = 10;
+        int b = 66;
         LocalDate tanggalLahir1 = LocalDate.of(2004, 12, 10);
 
         String nama2 = "Rudi";
