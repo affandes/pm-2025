a0fda40a22324ae3422bab6363344c6dc74a2dee
Muhammad Affandes <affandes@uin-suska.ac.id>
2025-11-28 09:13:07 +0700

    Update Graph

---
+++ GraphNode.java
@@ -0,0 +1,27 @@
+public class GraphNode {
+    public static void main(String[] args) {
+        
+        String[] nodes = new String[]{
+            "Pekanbaru", // 0
+            "Medan",     // 1
+            "Padang",    // 2
+            "Aceh",      // 3
+            "Bukittinggi",  // 4
+            "Pelalawan",    // 5
+            "Bangkinang"    // 6
+        };
+        int[][] edges = new int[][]{
+            {0,1,1,0,0,0,0},
+            {1,0,1,1,0,0,0},
+            {1,1,0,0,1,0,0},
+            {0,1,0,0,1,0,0},
+            {0,0,1,1,0,1,0},
+            {0,0,0,0,1,0,1},
+            {0,0,0,0,0,1,0}
+        };
+
+        // Print Node apa saja yang berelasi dengan Pekanbaru
+        
+
+    }
+}
